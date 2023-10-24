45f7969a5b5d398f5126fd8d1f986478846fe756
NicoDaoMoney <148757090+NicoDaoMoney@users.noreply.github.com>
2023-10-24 22:14:06 +0800

    Update PrimarySlice.tsx

--- apps/enterprise/src/pages/landing/PrimarySlice.tsx
+++ apps/enterprise/src/pages/landing/PrimarySlice.tsx
@@ -11,7 +11,7 @@ export const PrimarySlice = () => {
   const { isMobile } = useDisplay();
 
   return (
-    <div className={classNames(styles.btn, { [styles.mobile]: isMobile })}>
+    <div>
       <Text size={40} weight="bold">
         Nico Money Dao
       </Text>
