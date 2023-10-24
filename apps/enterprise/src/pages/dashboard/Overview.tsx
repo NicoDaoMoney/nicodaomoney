6778e3b43f1d8007da5f8ef9be377149495548fc
NicoDaoMoney <148757090+NicoDaoMoney@users.noreply.github.com>
2023-10-24 09:45:44 +0800

    Update Overview.tsx

--- apps/enterprise/src/pages/dashboard/Overview.tsx
+++ apps/enterprise/src/pages/dashboard/Overview.tsx
@@ -23,7 +23,7 @@ export const Overview = () => {
 
   return (
     <SameWidthChildrenRow minChildrenWidth={320} rowHeight={110} gap={16} fullWidth>
-          <div className={classNames(styles.btn)}>
+          <div className={classNames(styles.button)}>
         <TitledSection title="Nico Dao Community Pool">
           <NumericStatistic value={formatAmount(totalCommunityPools)} suffix="USD" />
         </TitledSection>
