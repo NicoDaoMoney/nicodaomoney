d699af14531b6daa1bf3a1e0594ffa1930899b65
NicoDaoMoney <148757090+NicoDaoMoney@users.noreply.github.com>
2023-10-25 00:11:52 +0800

    Update darkTheme.ts

--- apps/enterprise/src/lib/ui/theme/darkTheme.ts
+++ apps/enterprise/src/lib/ui/theme/darkTheme.ts
@@ -1,42 +1,43 @@
 import { DefaultTheme } from 'styled-components';
-import { HSLA } from 'lib/ui/colors/HSLA';
-import { sharedColors } from './shared';
-import { generateLabelColorGetter } from '../colors/generateLabelColorGetter';
-
-const backgroundHue = 60;
-const backgroundSaturation = 28;
-const backgroundLightness = 0;
-const backgroundAlpa = 0.19;
 
 export const darkTheme: DefaultTheme = {
   name: 'dark',
   colors: {
-    ...sharedColors,
-
-    success: new HSLA(130, 56, 52),
-    alert: new HSLA(0, 79, 63),
-    idle: new HSLA(32, 79, 48),
-
-    foreground: new HSLA(backgroundHue, backgroundSaturation, backgroundLightness + 3, backgroundAlpa),
-    background: new HSLA(0, 0, 0, 1),
-    text: new HSLA(0, 0, 100, 1),
-    textSupporting: new HSLA(0, 0, 100, 1),
-    textShy: new HSLA(0, 0, 100, 1),
-
-    mist: new HSLA(233, 100, 9, 0.66),
-    mistExtra: new HSLA(55, 93, 52, 0.6),
-
-    overlay: new HSLA(backgroundHue, backgroundSaturation, 1, 0.8),
-
-    getLabelColor: generateLabelColorGetter({
-      saturation: 56,
-      lightness: 52,
-    }),
-
-    contrast: new HSLA(0, 0, 100),
+    success: 'deepskyblue',
+    alert: 'darkviolet',
+    idle: 'blue',
+    foreground: 'white',
+    background: 'black',
+    text: 'white',
+    textSupporting: 'white',
+    textShy: 'white',
+    mist: 'lightgray',
+    mistExtra: 'rgba(85, 93, 132, 0.6)',
+    overlay: 'hsla(60, 28%, 100%, 0.8)',
+    contrast: 'white',
   },
   shadows: {
     small: 'rgb(248 231 23 / 20%) 0px 0px 0px 1px, rgb(248 231 23 / 20%) 0px 2px 4px',
-    medium: 'rgb(248 231 23 / 10%) 0px 0px 0px 1px, rgb(248 231 23 / 20%) 0px 3px 6px, rgb(248 231 23 / 40%) 0px 9px 24px;',
+    medium: 'rgb(248 231 23 / 10%) 0px 0px 0px 1px, rgb(248 231 23 / 20%) 0px 3px 6px, rgb(248 231 23 / 40%) 0px 9px 24px',
+  },
+};
+
+export const gradientBackground = {
+  background: 'linear-gradient(300deg, deepskyblue, darkviolet, blue)',
+  backgroundSize: '180% 180%',
+  animation: 'gradient-animation 18s ease infinite',
+};
+
+export const gradientKeyframes = {
+  '@keyframes gradient-animation': {
+    '0%': {
+      backgroundPosition: '0% 50%',
+    },
+    '50%': {
+      backgroundPosition: '100% 50%',
+    },
+    '100%': {
+      backgroundPosition: '0% 50%',
+    },
   },
 };
